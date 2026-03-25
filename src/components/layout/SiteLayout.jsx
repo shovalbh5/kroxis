b937545dc6d35897db89a48656578bb86e3164fa
Shoval Berenshtein <shovalbh5@gmail.com>
2026-03-25 18:22:01 +0000

    File changes

--- src/components/layout/SiteLayout.jsx
+++ src/components/layout/SiteLayout.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import CartDrawer from './CartDrawer';
 import AccessibilityMenu from '../accessibility/AccessibilityMenu';
 
 export default function SiteLayout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
